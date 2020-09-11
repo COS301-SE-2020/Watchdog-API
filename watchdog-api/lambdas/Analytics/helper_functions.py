1a1b17a8618823cb895348328342e10b2cba6adb
Jonathen Sundy <jonathensundy@gmail.com>
2020-09-11 02:32:16 +0200

    DetectIntuder cleanup
    Cleanup the lambda function by encapsulating the functions into a separate file.

---
+++ watchdog-api/lambdas/Analytics/helper_functions.py
@@ -0,0 +1,326 @@
+import boto3
+import json
+import base64
+import os
+from boto3.dynamodb.conditions import Key
+from datetime import datetime, timedelta
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
+from botocore.client import Config
+
+
+s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
+rekognition = boto3.client('rekognition', region_name=os.environ['REKOGNITION_REGION'])
+
+
+def from_dynamodb_to_json(item):
+    d = TypeDeserializer()
+    return {k: d.deserialize(value=v) for k, v in item.items()}    
+    
+
+def get_training_locations(user_id):
+    client = boto3.client('dynamodb', region_name='af-south-1')
+            
+    response = client.query(
+        TableName='UserData',
+        ProjectionExpression='preferences.notifications, preferences.security_level',
+        KeyConditionExpression=f'user_id = :user_id',
+        ExpressionAttributeValues={
+            ':user_id': {"S": user_id}
+        }
+    )
+    
+    data = from_dynamodb_to_json(response['Items'][0])
+    preferences = data['preferences']['notifications']
+    security_level = int(data['preferences']['security_level'])
+    
+    response = client.query(
+        TableName='Artefacts',
+        ProjectionExpression='profiles',
+        KeyConditionExpression=f'user_id = :user_id',
+        ExpressionAttributeValues={
+            ':user_id': {"S": user_id}
+        }
+    )
+    
+    data = from_dynamodb_to_json(response['Items'][0])
+    training_set = data['profiles']
+
+    print("(3. Get user details): preferences:"+str(preferences)+" training set images:"+str(training_set) +" security_level:"+str(security_level))
+    return preferences, training_set, security_level
+
+
+def face_detected(img_name):
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object':{
+                'Bucket':os.environ['BUCKET'],
+                'Name':img_name
+            }
+        },
+        MinConfidence=90
+    )
+    
+    for label in response['Labels']:
+        if label['Name'] == "Person" and label['Confidence'] > 90:
+            print("face detected in image "+img_name)
+            return True
+    print("face NOT detected in image "+img_name)
+    return False
+
+
+def is_owner(sources, target, bucket):
+    response = False
+    if len(sources) > 0:
+        index = -1
+        for source in sources:
+            index = index + 1
+            response = rekognition.compare_faces(
+                SourceImage={
+                    'S3Object': {
+                        'Bucket': bucket,
+                        'Name': source['key']
+                    }
+                },
+                TargetImage={
+                    'S3Object': {
+                        'Bucket': bucket,
+                        'Name': target
+                    }
+                }
+                # QualityFilter='NONE'|'AUTO'|'LOW'|'MEDIUM'|'HIGH'
+            )
+            if len(response['FaceMatches']) > 0:
+                print("(5.a compare faces (training set & detected image)): Owner Identified:{source image:"+source['key']+", target image:"+target+"}")
+                return True, index, source
+    print("(5.a compare faces (training set & detected image)): Owner NOT Identified: target image:"+target)
+    return False, -1, None
+
+
+def send_sms(number, link, message=None):
+    sns = boto3.client("sns", region_name="eu-west-1")
+    if message is None:
+        message = "Please be aware that you may be experiencing a potential breach! View the detected image at the following link: "+link
+    else:
+        message = message + " View the detected image at the following link: "+link
+    # if phone[0] is not plus
+    # add +27 and remove the first digit
+    sns.publish(
+        PhoneNumber=number, 
+        Message=message,
+        MessageAttributes={
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': 'Watchdog'
+            },
+            'AWS.SNS.SMS.SMSType': {
+                'DataType': 'String',
+                'StringValue': 'Transactional'
+            }
+        }
+    )
+    
+def send_email(recipient, link, message=None):
+    if message is None:
+        data = "Intruder Detected on property!"
+        message = "Please note that detection of a possible intruder has been captured on your camera feed."
+    else:
+        data = "Someone in your watchlist has been detected"
+
+    BODY_HTML = f"""<html>
+    <head></head>
+    <body>
+        <h1>Intruder Detected</h1>
+        <p>
+            {message}
+        </p>
+        <p>
+            Detected Image:
+        </p>
+        <p>
+            
+            <img src="{link}" alt="Image Expired">
+        </p>
+    </body>
+    </html>
+    """
+    client = boto3.client('ses',region_name='eu-west-1')
+    response = client.send_email(
+        Destination={
+            'ToAddresses': [
+                recipient,
+            ],
+        },
+        Message={
+            'Body': {
+                'Html': {
+                    'Charset': "UTF-8",
+                    'Data': BODY_HTML,
+                },
+            },
+            'Subject': {
+                'Charset': "UTF-8",
+                'Data': "",
+            },
+        },
+        Source="lynk.solutions.tuks@gmail.com",
+    )
+
+
+def generate_presigned_link(key):
+    response = s3.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': 'intruder.analysis',
+            'Key': key
+        },
+        ExpiresIn=3600
+    )
+    
+    return response   
+
+
+def send_notification(preferences, target, send_to_security=True, custom_message=None):
+    print("(5.b Send notificaiton to Owner & Security Company)")
+    link = generate_presigned_link(target)
+    type = preferences['type']
+    if type == 'sms':
+        message = "Intruder detected"
+        send_sms(preferences['phone'], link, custom_message)
+        print(f"(5.b.1. Notify Owner) SMS:{message}; TO:{preferences['phone']}")
+    elif type == 'email':
+        send_email(preferences['email'], link, custom_message)
+        print(f"(5.b.1. Notify Owner) Email recipient:{preferences['email']}")
+    elif type == 'push':
+        pass
+    if send_to_security:
+        if len(preferences['security_company'])>0:
+            security_message = "One of your clients may have a potential breach! Please contact "+preferences['phone']+" immediately!"
+            print(f"(5.b.2. Notify security): SMS:{security_message}; TO:{preferences['security_company']}")
+            send_sms(preferences['security_company'], link)
+
+
+def get_meta_data_from_event(event):
+    bucket = os.environ['BUCKET']
+    record = event['Records'][0]['s3']
+    key = record['object']['key']
+    metadata = s3.head_object(Bucket=bucket, Key=key)
+    metadata = metadata['ResponseMetadata']['HTTPHeaders']
+    return metadata['x-amz-meta-uuid'], key, metadata['x-amz-meta-camera_id'], metadata['x-amz-meta-timestamp'], metadata['x-amz-meta-token']
+
+
+def remove_s3_object(key):
+    response = s3.delete_object(
+        Bucket = os.environ['BUCKET'],
+        Key=key
+    )
+    print(f"(3. Delete detected false-positive): Key:{key}")
+
+
+def add_detected_image_to_artefacts(key, uuid, camera_id, timestamp, io, source):
+    client = boto3.resource('dynamodb', region_name='af-south-1')
+    table = client.Table('Artefacts')
+    
+    data = {
+        'key': key,
+        'timestamp':timestamp,
+        'metadata': {
+            'camera_id':camera_id
+        }
+    }
+    if io:
+        field = 'whitelist'
+        data.update({'aid':source['aid']})
+    else:
+        field = 'blacklist'
+
+    response = table.update_item(
+        Key={
+            'user_id': uuid,
+        },
+        UpdateExpression=f"SET {field} = list_append({field}, :i)",
+        ExpressionAttributeValues={
+            ":i": [
+                data
+                    # 'aid': hash(f'{key}-{uuid}'),
+            ]
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    print(f"(7. Add detected image key to artifacts): Key:{key};    is owner:{io};    field:{field}    camera id:{camera_id}")
+    return response
+
+def append_log(message, user_id, token, key, camera_id):
+    timestamp = str(datetime.now().timestamp())
+    data = {
+        'tag': 'detected',
+        "message":message,
+        "timestamp": timestamp,
+    }
+
+    client = boto3.resource('dynamodb',region_name='af-south-1')
+    table = client.Table('UserData')
+    
+    response = table.update_item(
+        Key= {"user_id": user_id},
+        UpdateExpression='SET logs = list_append(logs, :obj)',
+        ExpressionAttributeValues={
+            ":obj": [data]
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    
+    print(f"(6. Append Log): Log message:{message};  timestamp:{timestamp}")
+
+
+def get_detected_frames(user_id):
+    time_gap = datetime.now() - timedelta(minutes=10)
+    time_gap = str(time_gap.timestamp())
+    
+    client = boto3.client('dynamodb', region_name='af-south-1')
+    
+    response = client.query(
+        TableName='Artefacts',
+        ProjectionExpression='blacklist, whitelist',
+        KeyConditionExpression=f'user_id = :user_id',
+        ExpressionAttributeValues={
+            ':user_id': {"S": user_id}
+        }
+    )
+    detected_frames = from_dynamodb_to_json(response['Items'][0])
+    detected_frames = detected_frames['whitelist']+detected_frames['blacklist']
+    print("TEMP: response from getting whitelist and blacklist frames to check for uniqueness: "+str(detected_frames))
+    
+    frames_to_check = []
+    for i in detected_frames:
+        if i['timestamp'] > time_gap:
+            frames_to_check.append(i)
+    print(f"(2.2.1 Detected Frames in the past 10 min):  frames:{frames_to_check}")
+    
+    return frames_to_check
+        
+
+def is_detected_unique(user_id, target):
+    print(f"(2.2 Get detected frames to see uniqueness): user_id:{user_id}; detected image:{target}")
+    detected_frames = get_detected_frames(user_id)
+    if len(detected_frames) > 0:
+        for i in detected_frames:
+            response = rekognition.compare_faces(
+                SourceImage={
+                    'S3Object': {
+                        'Bucket': os.environ['BUCKET'],
+                        'Name': target
+                    }
+                },
+                TargetImage={
+                    'S3Object': {
+                        'Bucket': os.environ['BUCKET'],
+                        'Name': i['key']
+                    }
+                }
+                # QualityFilter='NONE'|'AUTO'|'LOW'|'MEDIUM'|'HIGH'
+            )
+            if len(response['FaceMatches']) > 0:
+                print(f"(2.2.1 Detected image Uniqueness): Detected image is not unique: detected image:{target};   source image:{i['key']}")
+                return False
+    print(f"(2.2.1 Detected image Uniqueness): Detected image is unique: detected image:{target};")
+    return True
--- watchdog-api/lambdas/Analytics/lambda_function.py
+++ watchdog-api/lambdas/Analytics/lambda_function.py
@@ -1,212 +1,57 @@
-import boto3
-import json
-from copy import deepcopy
-import os
-from random import randint
-from boto3.dynamodb.conditions import Key
-from datetime import datetime, timedelta, date
-from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
-from botocore.client import Config
-
-
-s3 = boto3.client('s3', config=Config(signature_version='s3v4'), region_name='eu-west-1')
-dynamo_client = boto3.client('dynamodb', region_name='af-south-1')
-
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            return float(o)
-        return super(DecimalEncoder, self).default(o)
-
-def from_dynamodb_to_json(item):
-    d = TypeDeserializer()
-    return {k: d.deserialize(value=v) for k, v in item.items()}
-
-
-def get_profile_data(user_id):
-    '''
-        get profiles and whitelist data from database
-    '''
-    response = dynamo_client.query(
-        TableName='Artefacts',
-        ProjectionExpression='profiles, whitelist',
-        KeyConditionExpression=f'user_id = :user_id',
-        ExpressionAttributeValues={
-            ':user_id': {"S": user_id}
-        }
-    )
-    
-    data = from_dynamodb_to_json(response['Items'][0])
-    profiles = data['profiles']
-    whitelist = data['whitelist']
-
-    return profiles, whitelist
-
-
-def get_graph_template_data(end_date, time_scale):
-    '''
-        get data skeleton list for graph plugin
-            1. get the time intervals
-            2. get data skeleton given intervals
-    '''
-    
-    intervals = -1
-    HOURS = 0
-    DAYS = 0
-    WEEKS = 0
-
-    if time_scale == "DAILY":
-        intervals = 24   # 24 hours per day
-        HOURS = 1
-    elif time_scale == "WEEKLY":
-        intervals = 7   # 7 days per week
-        DAYS = 1
-    elif time_scale == "MONTHLY":
-        intervals = 4   # 4 weeks per month
-        WEEKS = 1
-    
-    end_date = date.fromtimestamp(float(end_date))
-    end_date = datetime(end_date.year, end_date.month, end_date.day)
-    x_axis = []
-    data = []
-    inter = []
-    time_gap = timedelta(hours=HOURS, days=DAYS, weeks=WEEKS)
-    for i in range(intervals-1, -1, -1):
-        x_step = end_date - (i*time_gap)
-        inter.append(str(x_step.timestamp()))
-        data.append(
-            {
-                "images":[],
-                "x": x_step.strftime("%d %B %Y"),
-                "y":0
-            }
-        )
-    
-    return data, inter
-
-def binsearch(t, key, low = 0, high = 0):
-	high = len(t) - 1
-	while low < high:
-		mid = (low + high)//2
-		if float(t[mid]) < float(key):
-			low = mid + 1
-		else:
-			high = mid
-	if float(t[low]) > float(key) and low > 0:
-	    low = low-1
-	return low if key >= t[0] else -1
-
-
-def generate_presigned_link(key):
-    #generating a presigned link for S3 bucket 
-    try:
-        response = s3.generate_presigned_url(
-            'get_object',
-            Params={
-                'Bucket': os.environ['BUCKET'],
-                'Key': key
-            },
-            ExpiresIn=3600
-        )
-    except ClientError as e:
-        logging.error(e)
-        return None
-
-    return response
-
-
-def get_location_from_camera_id(user_id, camera_id):
-    response = dynamo_client.query(
-        TableName='UserData',
-        ProjectionExpression='control_panel',
-        KeyConditionExpression=f'user_id = :user_id',
-        ExpressionAttributeValues={
-            ':user_id': {"S": user_id}
-        }
-    )
-    data = from_dynamodb_to_json(response['Items'][0])
-    data = data['control_panel']
-    location = None
-    for site in data:
-        for loc in data[site]:
-            for cam in data[site][loc]:
-                if cam == camera_id:
-                    location = loc
-                    break
-    return location
-
-
-def populate_graph_data(graph_data, whitelist, intervals, user_id):
-    '''
-        populate graph skeleton with data from whitelist
-        1. check each whitelist and see if it is in correct range
-        2. if so, insert image object into correct profileat specific interval
-    '''
-    for detected_owner in whitelist:
-        index = binsearch(intervals, detected_owner['timestamp'])
-        print("index of detected owner "+str(index))
-        if index != -1:
-            for profile in graph_data:
-                if profile['aid'] == detected_owner['aid']:
-                    profile['data'][index]['images'].append(
-                        {
-                            "link": generate_presigned_link(detected_owner['key']),
-                            "timestamp": detected_owner['timestamp'],
-                            "location": get_location_from_camera_id(user_id, detected_owner['metadata']['camera_id']),
-                            "camera_id": detected_owner['metadata']['camera_id']
-                        }
-                    )
-                    profile['data'][index]['y'] = profile['data'][index]['y']+1
-                    break
-    return graph_data
-
-
-def error(msg):
-    return {
-        "status": "ERROR",
-        "message": f'Encountered an Unexpected Error: {msg}'
-    }
-    
-# Bundle the response with an OK
-def success(msg, extra={}):
-    return {
-        "status": "OK",
-        "message": f'Operation Completed with Message: {msg}',
-        "data": extra
-    }
+from helper_functions import *
 
 
 def lambda_handler(event, context):
-    route = event['resource']
-    params = event['queryStringParameters']
-    user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
+    # get parameters from s3 object
+    uuid, target, camera_id, timestamp, token = get_meta_data_from_event(event)
+    print(f"(1. Parameters): Detected Image: {target}; User ID: {uuid}; camera id:{camera_id}   timestamp:{timestamp}   token:{token}")
+    bucket = os.environ['BUCKET']
     
-    end_date = params['end_date']
-    time_scale = params['time_scale']
-    try:
-        # profile analytics
-        # 1. preprocess data - load profiles with data structure of graph
-        profiles, whitelist = get_profile_data(user_id)
-        print(f"(1.1 get profile data): profiles:{profiles}    whitelist:{whitelist}")
-        # 2. load data - go through whitelist images and add the images to specified list
-        graph_skeleton, intervals = get_graph_template_data(end_date, time_scale)
-        print(f"(2.1 ): graph data skeleton:{graph_skeleton}  intervals: {intervals}")
-        graph_data = []
-        for profile in profiles:
-            graph_data.append(
-                {
-                    "id":profile["name"],
-                    "aid":profile["aid"],
-                    "color":f"hsl({randint(0,10)*36},50%,50%)",
-                    "data":deepcopy(graph_skeleton)
-                }
-            )
-
-        print(f"(2. graph skeleton data with profiles): data:{graph_data}")
-        graph_data = populate_graph_data(graph_data, whitelist, intervals, user_id)
-        print(f"(3. graph populated data): graph data:{graph_data}")
-        resp = success(f"Successfully retrieved graph data for interval {time_scale}", graph_data)
-    except Exception as e:
-        resp = error(f'Could not complete Dynamo Operation due to Error: {e}')
+    # define rekognition resource
+    owner = False
+    response = "Person was identified within the last 10 minutes and will not be processed again"
+    if face_detected(target) and is_detected_unique(uuid, target):
+        # face has been detected in detected image - now compare it to other images from user uploads
+        preferences, training_set, security_level = get_training_locations(uuid)
         
-    return {"statusCode":200, "headers": {"Access-Control-Allow-Origin": "*"}, "body":json.dumps(resp)}
+        io, index, source = is_owner(training_set, target, bucket)
+        
+        if security_level == 0: # Disarmed ( no notifications are sent)
+            print("(4. security level): level:0;   description:Disarmed;   action:no notifications are sent")
+            log_message = "Watchdog has identified movement"
+        elif security_level == 1:   # Recognised Only (so intruder notifications are sent)
+            print("(4. security level): level:1;   description:Recognised Only;   action:notifications are sent if the detected image is an owner")
+            if not io:
+                send_notification(preferences, target)
+                log_message = "Watchdog has identified a possible intruder, and has sent out a notificaiton!"
+            else:
+                log_message = "Watchdog has identified the owner in your feed!"
+                if training_set[index]['monitor']['watch']:
+                    send_notification(preferences, target, False, training_set[index]['monitor']['custom_message'])
+        else:   # Armed (notifications are sent for any face detected)
+            print("(4. security level): level:2;   description:Armed;   action:notifications are sent")
+            send_notification(preferences, target)
+            log_message = "Watchdog has identified a face in your feed. if this is your face, consider changing your security to level 1, security has been notified"
+        
+        append_log(log_message, uuid, token, target, camera_id)
+        response = "Intruder Detected!"  
+
+        # add to Dynamo DB - artifacts
+        add_detected_image_to_artefacts(target, uuid, camera_id, timestamp, io, source)
+    else:
+        # person is detected within 10 minutes, therefore, do not save image
+        remove_s3_object(target)
+
+
+    resp = {
+        "response": response
+    }
+    respObj = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": resp
+    }
+
+    return respObj
