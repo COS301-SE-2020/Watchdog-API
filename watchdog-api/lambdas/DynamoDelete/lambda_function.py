0e3d7d0046747d1e127fa05ba5eecc05de7994c7
Jonathen Sundy <jonathensundy@gmail.com>
2020-09-15 01:47:16 +0200

    intelligent deleting
    when deleting images from profiles, move similar images to the whitelist so that the user can add them to profiles again.

--- watchdog-api/lambdas/DynamoDelete/lambda_function.py
+++ watchdog-api/lambdas/DynamoDelete/lambda_function.py
@@ -1,18 +1,20 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
-import decimal
+# import decimal
 from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            return float(o)
-        return super(DecimalEncoder, self).default(o)
+
+# class DecimalEncoder(json.JSONEncoder):
+#     def default(self, o):
+#         if isinstance(o, decimal.Decimal):
+#             return float(o)
+#         return super(DecimalEncoder, self).default(o)
 
 def from_dynamodb_to_json(item):
     d = TypeDeserializer()
     return {k: d.deserialize(value=v) for k, v in item.items()}
+
 
 # Bundle the Response with an ERROR
 def error(msg, extra={}):
@@ -21,7 +23,8 @@ def error(msg, extra={}):
         "message": f'Encountered an Unexpected Error: {msg}',
         **extra
     }
-    
+
+
 # Bundle the response with an OK
 def success(msg, extra={}):
     return {
@@ -29,43 +32,148 @@ def success(msg, extra={}):
         "message": f'Operation Completed with Message: {msg}',
         "data": {**extra}
     }
-    
+
+
+def remove_profile_at_index(index, user_id):
+    index = int(index)
+    dynamo_client = boto3.client('dynamodb', region_name='af-south-1')
+    dynamo_resource = boto3.resource('dynamodb', region_name='af-south-1')
+    # get profile data
+    response = dynamo_client.query(
+        TableName='Artefacts',
+        KeyConditionExpression=f'user_id = :user_id',
+        ProjectionExpression='profiles',
+        ExpressionAttributeValues={
+            ':user_id': {"S": user_id}
+        }
+    )
+    print("1. returned: " + str(response['Items'][0]))
+    # response = json.loads(response)
+    data = from_dynamodb_to_json(response['Items'][0])
+    _key = data['profiles'][index]['key']
+    table = dynamo_resource.Table('Artefacts')
+    res = table.update_item(
+        Key={
+            "user_id": user_id
+        },
+        UpdateExpression=f'REMOVE profiles[{index}]',
+        ConditionExpression=f'profiles[{index}].#key = :image_key',
+        ReturnValues="UPDATED_NEW",
+        ExpressionAttributeValues={
+            ':image_key': _key
+        },
+        ExpressionAttributeNames={
+            '#key': "key"
+        }
+    )
+    print("profile: " + str(data['profiles'][index]))
+    return data['profiles'][index]
+
+
+def move_whitelist_images(profile, user_id):
+    """
+        move and remove all images in the whitelist that have the same aid
+        (same person) to the blacklist so that the user can tag them again
+        if they want to.
+    """
+    # get whitelist images
+    dynamo_client = boto3.client('dynamodb', region_name='af-south-1')
+    response = dynamo_client.query(
+        TableName='Artefacts',
+        KeyConditionExpression=f'user_id = :user_id',
+        ProjectionExpression='whitelist',
+        ExpressionAttributeValues={
+            ':user_id': {"S": user_id}
+        }
+    )
+    data = from_dynamodb_to_json(response['Items'][0])
+    data = data['whitelist']
+    # find all occurences of given aid in profile with whitelist
+    count = 0
+    dynamo_resource = boto3.resource('dynamodb', region_name='af-south-1')
+    table = dynamo_resource.Table('Artefacts')
+
+    for i in range(len(data) - 1, -1, -1):
+        if str(profile['aid']) == str(data[i]['aid']):
+            # try and remove the whitelist image in the database given the index
+            try:
+                res = table.update_item(
+                    Key={
+                        "user_id": user_id
+                    },
+                    UpdateExpression=f'REMOVE whitelist[{i}]',
+                    ConditionExpression=f'whitelist[{i}].#key = :image_key',
+                    ReturnValues="UPDATED_NEW",
+                    ExpressionAttributeValues={
+                        ':image_key': data[i]['key']
+                    },
+                    ExpressionAttributeNames={
+                        '#key': "key"
+                    }
+                )
+            except dynamo_resource.meta.client.exceptions.ConditionalCheckFailedException as e:
+                print(e)
+                return False
+            # add the whitelist record to the blacklist
+            response = table.update_item(
+                Key={
+                    'user_id': user_id,
+                },
+                UpdateExpression="SET blacklist = list_append(blacklist, :i)",
+                ExpressionAttributeValues={
+                    ":i": [
+                        {
+                            "key": data[i]['key'],
+                            "timestamp": data[i]['timestamp'],
+                            "metadata": data[i]['metadata']
+                        }
+                    ]
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            print(f"Moved whitelist image {data[i]['key']} to the blacklist")
+            count += 1
+    print(f"Amount of whitelist images removed and moved to the blacklist: {count}")
+    return True
+
+
 def lambda_handler(event, context):
-    client = boto3.resource('dynamodb', region_name = 'af-south-1')
-    
+    client = boto3.resource('dynamodb', region_name='af-south-1')
+
     route = event['resource']
     params = event['queryStringParameters']
-    user_id = event["requestContext"]["authorizer"]["claims"]["sub"] 
-    
+    user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
+
     resp = {}
-        
+
     try:
-        #the identities route to remove whitelisted images and profiles from the HCP 
+        # the identities route to remove whitelisted images and profiles from the HCP
         if '/identities' in route:
             index = params['index']
-            parameters = {
-                "Key": {"user_id": user_id},
-                "UpdateExpression": f'REMOVE profiles[{index}]',
-                "ReturnValues": "UPDATED_NEW"
-            }
-            table = client.Table('Artefacts')
-            data = table.update_item(**parameters)
-            resp = success(f'DELETE completed successfully on item: {index}', extra=data)
-            
+            print("remove index " + index)
+            dynamo_client = boto3.client('dynamodb', region_name='af-south-1')
+            # get profile data
+            profile = remove_profile_at_index(index, user_id)
+            print(f"profile {profile}")
+            is_valid = False
+            while (is_valid == False):
+                is_valid = move_whitelist_images(profile, user_id)
+            resp = success(f'DELETE completed successfully on item: {index}', extra={})
         elif '/cameras' in route:
             site_id = params['site_id']
             location = params['location']
             camera_id = params['camera_id']
-            print(f"(1. Parameters): route:'/cameras'   site_id:{site_id}   location:{location}    camera_id:{camera_id}")
+            print(
+                f"(1. Parameters): route:'/cameras'   site_id:{site_id}   location:{location}    camera_id:{camera_id}")
             table = client.Table('UserData')
             resp = table.update_item(
-                Key =  {
+                Key={
                     "user_id": user_id
                 },
-                UpdateExpression = f'REMOVE control_panel.#site_id.#location.#camera_id',
-                ReturnValues = "UPDATED_NEW",
-                ExpressionAttributeNames = {
-                    '#site_id':site_id,
+                UpdateExpression=f'REMOVE control_panel.#site_id.#location.#camera_id',
+                ReturnValues="UPDATED_NEW",
+                ExpressionAttributeNames={
+                    '#site_id': site_id,
                     "#location": location,
                     '#camera_id': camera_id
                 }
@@ -78,22 +186,22 @@ def lambda_handler(event, context):
             '''
             site_id = params['site_id']
             location = params['location']
-            
+
             print(f"(1. Parameters): route:'/cameras'   site_id:{site_id}   location:{location}")
             table = client.Table('UserData')
             resp = table.update_item(
-                Key =  {
+                Key={
                     "user_id": user_id
                 },
-                UpdateExpression = f'REMOVE control_panel.#site_id.#location',
-                ReturnValues = "UPDATED_NEW",
-                ExpressionAttributeNames = {
-                    '#site_id':site_id,
+                UpdateExpression=f'REMOVE control_panel.#site_id.#location',
+                ReturnValues="UPDATED_NEW",
+                ExpressionAttributeNames={
+                    '#site_id': site_id,
                     "#location": location
                 }
             )
             resp = success(f'DELETE completed successfully for location {location} in site {site_id}', extra=resp)
-            
+
     except ClientError as e:
         resp = error(f'An unexpected Client Error occured: {e}', {'data': e})
         print(str(resp))
